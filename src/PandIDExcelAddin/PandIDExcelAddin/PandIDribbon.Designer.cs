ac03f821f7f8b0dd8a685ebb446880bc8abbb4ad
peer23peer <spijker.jelle@gmail.com>
2015-10-20 14:16:18 +0200

    added icons etc

--- src/PandIDExcelAddin/PandIDExcelAddin/PandIDribbon.Designer.cs
+++ src/PandIDExcelAddin/PandIDExcelAddin/PandIDribbon.Designer.cs
@@ -34,31 +34,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tab1 = this.Factory.CreateRibbonTab();
+            this.tabPandID = this.Factory.CreateRibbonTab();
             this.groupProject = this.Factory.CreateRibbonGroup();
+            this.dropDownProject = this.Factory.CreateRibbonDropDown();
+            this.labelCustomer = this.Factory.CreateRibbonLabel();
+            this.labelClass = this.Factory.CreateRibbonLabel();
             this.groupDiagram = this.Factory.CreateRibbonGroup();
             this.dropDownDiagrams = this.Factory.CreateRibbonDropDown();
-            this.dropDownProject = this.Factory.CreateRibbonDropDown();
-            this.buttonEditProject = this.Factory.CreateRibbonButton();
-            this.tab1.SuspendLayout();
+            this.groupSystem = this.Factory.CreateRibbonGroup();
+            this.dropDownFluid = this.Factory.CreateRibbonDropDown();
+            this.editBoxTemp = this.Factory.CreateRibbonEditBox();
+            this.editBoxPressure = this.Factory.CreateRibbonEditBox();
+            this.editBoxMaxVelocity = this.Factory.CreateRibbonEditBox();
+            this.groupPipes = this.Factory.CreateRibbonGroup();
+            this.dropDownNorm = this.Factory.CreateRibbonDropDown();
+            this.dropDownDia = this.Factory.CreateRibbonDropDown();
+            this.editBoxLength = this.Factory.CreateRibbonEditBox();
+            this.editBoxHeight = this.Factory.CreateRibbonEditBox();
+            this.buttonAddPipe = this.Factory.CreateRibbonButton();
+            this.Valves = this.Factory.CreateRibbonGroup();
+            this.dropDownType = this.Factory.CreateRibbonDropDown();
+            this.dropDownComponent = this.Factory.CreateRibbonDropDown();
+            this.buttonAddComp = this.Factory.CreateRibbonButton();
+            this.tabPandID.SuspendLayout();
             this.groupProject.SuspendLayout();
             this.groupDiagram.SuspendLayout();
+            this.groupSystem.SuspendLayout();
+            this.groupPipes.SuspendLayout();
+            this.Valves.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tab1
+            // tabPandID
             // 
-            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
-            this.tab1.Groups.Add(this.groupProject);
-            this.tab1.Groups.Add(this.groupDiagram);
-            this.tab1.Label = "TabAddIns";
-            this.tab1.Name = "tab1";
+            this.tabPandID.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tabPandID.Groups.Add(this.groupProject);
+            this.tabPandID.Groups.Add(this.groupDiagram);
+            this.tabPandID.Groups.Add(this.groupSystem);
+            this.tabPandID.Groups.Add(this.groupPipes);
+            this.tabPandID.Groups.Add(this.Valves);
+            this.tabPandID.Label = "P&ID Toolbox";
+            this.tabPandID.Name = "tabPandID";
             // 
             // groupProject
             // 
             this.groupProject.Items.Add(this.dropDownProject);
-            this.groupProject.Items.Add(this.buttonEditProject);
+            this.groupProject.Items.Add(this.labelCustomer);
+            this.groupProject.Items.Add(this.labelClass);
             this.groupProject.Label = "Project";
             this.groupProject.Name = "groupProject";
+            // 
+            // dropDownProject
+            // 
+            this.dropDownProject.Label = "Project";
+            this.dropDownProject.Name = "dropDownProject";
+            this.dropDownProject.SelectionChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.dropDownProject_SelectionChanged);
+            // 
+            // labelCustomer
+            // 
+            this.labelCustomer.Label = "Customer";
+            this.labelCustomer.Name = "labelCustomer";
+            // 
+            // labelClass
+            // 
+            this.labelClass.Label = "Classification Soc.";
+            this.labelClass.Name = "labelClass";
             // 
             // groupDiagram
             // 
@@ -70,41 +109,154 @@
             // 
             this.dropDownDiagrams.Label = "Diagrams";
             this.dropDownDiagrams.Name = "dropDownDiagrams";
+            this.dropDownDiagrams.SelectionChanged += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.dropDownDiagrams_SelectionChanged);
             // 
-            // dropDownProject
+            // groupSystem
             // 
-            this.dropDownProject.Label = "Project";
-            this.dropDownProject.Name = "dropDownProject";
+            this.groupSystem.Items.Add(this.dropDownFluid);
+            this.groupSystem.Items.Add(this.editBoxTemp);
+            this.groupSystem.Items.Add(this.editBoxPressure);
+            this.groupSystem.Items.Add(this.editBoxMaxVelocity);
+            this.groupSystem.Label = "System";
+            this.groupSystem.Name = "groupSystem";
             // 
-            // buttonEditProject
+            // dropDownFluid
             // 
-            this.buttonEditProject.Label = "Edit Project";
-            this.buttonEditProject.Name = "buttonEditProject";
+            this.dropDownFluid.Label = "Fluid";
+            this.dropDownFluid.Name = "dropDownFluid";
+            // 
+            // editBoxTemp
+            // 
+            this.editBoxTemp.Label = "Temperature";
+            this.editBoxTemp.Name = "editBoxTemp";
+            this.editBoxTemp.Text = null;
+            // 
+            // editBoxPressure
+            // 
+            this.editBoxPressure.Label = "Pressure";
+            this.editBoxPressure.Name = "editBoxPressure";
+            this.editBoxPressure.Text = null;
+            // 
+            // editBoxMaxVelocity
+            // 
+            this.editBoxMaxVelocity.Label = "Max. Velocity";
+            this.editBoxMaxVelocity.Name = "editBoxMaxVelocity";
+            this.editBoxMaxVelocity.Text = null;
+            // 
+            // groupPipes
+            // 
+            this.groupPipes.Items.Add(this.dropDownNorm);
+            this.groupPipes.Items.Add(this.dropDownDia);
+            this.groupPipes.Items.Add(this.editBoxLength);
+            this.groupPipes.Items.Add(this.editBoxHeight);
+            this.groupPipes.Items.Add(this.buttonAddPipe);
+            this.groupPipes.Label = "Pipes";
+            this.groupPipes.Name = "groupPipes";
+            // 
+            // dropDownNorm
+            // 
+            this.dropDownNorm.Label = "C-Norm";
+            this.dropDownNorm.Name = "dropDownNorm";
+            // 
+            // dropDownDia
+            // 
+            this.dropDownDia.Label = "DN";
+            this.dropDownDia.Name = "dropDownDia";
+            // 
+            // editBoxLength
+            // 
+            this.editBoxLength.Label = "Length";
+            this.editBoxLength.Name = "editBoxLength";
+            this.editBoxLength.Text = null;
+            // 
+            // editBoxHeight
+            // 
+            this.editBoxHeight.Label = "Height";
+            this.editBoxHeight.Name = "editBoxHeight";
+            this.editBoxHeight.ScreenTip = "Enter the difference in height between the start of the pipe and the end of the p" +
+    "ipe. Possitive means up. Units are in [m]";
+            this.editBoxHeight.Text = null;
+            // 
+            // buttonAddPipe
+            // 
+            this.buttonAddPipe.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.buttonAddPipe.Image = global::PandIDExcelAddin.Properties.Resources.pipe;
+            this.buttonAddPipe.Label = "Add pipe";
+            this.buttonAddPipe.Name = "buttonAddPipe";
+            this.buttonAddPipe.ShowImage = true;
+            // 
+            // Valves
+            // 
+            this.Valves.Items.Add(this.dropDownType);
+            this.Valves.Items.Add(this.dropDownComponent);
+            this.Valves.Items.Add(this.buttonAddComp);
+            this.Valves.Label = "Valves";
+            this.Valves.Name = "Valves";
+            // 
+            // dropDownType
+            // 
+            this.dropDownType.Label = "Component type";
+            this.dropDownType.Name = "dropDownType";
+            // 
+            // dropDownComponent
+            // 
+            this.dropDownComponent.Label = "Component";
+            this.dropDownComponent.Name = "dropDownComponent";
+            // 
+            // buttonAddComp
+            // 
+            this.buttonAddComp.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.buttonAddComp.Image = global::PandIDExcelAddin.Properties.Resources.valve;
+            this.buttonAddComp.Label = "Add component";
+            this.buttonAddComp.Name = "buttonAddComp";
+            this.buttonAddComp.ShowImage = true;
             // 
             // PandIDribbon
             // 
             this.Name = "PandIDribbon";
             this.RibbonType = "Microsoft.Excel.Workbook";
-            this.Tabs.Add(this.tab1);
+            this.Tabs.Add(this.tabPandID);
             this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.PandIDribbon_Load);
-            this.tab1.ResumeLayout(false);
-            this.tab1.PerformLayout();
+            this.tabPandID.ResumeLayout(false);
+            this.tabPandID.PerformLayout();
             this.groupProject.ResumeLayout(false);
             this.groupProject.PerformLayout();
             this.groupDiagram.ResumeLayout(false);
             this.groupDiagram.PerformLayout();
+            this.groupSystem.ResumeLayout(false);
+            this.groupSystem.PerformLayout();
+            this.groupPipes.ResumeLayout(false);
+            this.groupPipes.PerformLayout();
+            this.Valves.ResumeLayout(false);
+            this.Valves.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tabPandID;
         internal Microsoft.Office.Tools.Ribbon.RibbonGroup groupProject;
         internal Microsoft.Office.Tools.Ribbon.RibbonGroup groupDiagram;
         internal Microsoft.Office.Tools.Ribbon.RibbonDropDown dropDownProject;
-        internal Microsoft.Office.Tools.Ribbon.RibbonButton buttonEditProject;
         internal Microsoft.Office.Tools.Ribbon.RibbonDropDown dropDownDiagrams;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup groupSystem;
+        internal Microsoft.Office.Tools.Ribbon.RibbonDropDown dropDownFluid;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBoxTemp;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBoxPressure;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup groupPipes;
+        internal Microsoft.Office.Tools.Ribbon.RibbonDropDown dropDownNorm;
+        internal Microsoft.Office.Tools.Ribbon.RibbonDropDown dropDownDia;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBoxLength;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton buttonAddPipe;
+        internal Microsoft.Office.Tools.Ribbon.RibbonLabel labelCustomer;
+        internal Microsoft.Office.Tools.Ribbon.RibbonLabel labelClass;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBoxMaxVelocity;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup Valves;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton buttonAddComp;
+        internal Microsoft.Office.Tools.Ribbon.RibbonDropDown dropDownType;
+        internal Microsoft.Office.Tools.Ribbon.RibbonDropDown dropDownComponent;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBoxHeight;
     }
 
     partial class ThisRibbonCollection
--- src/PandIDExcelAddin/PandIDExcelAddin/PandIDribbon.cs
+++ src/PandIDExcelAddin/PandIDExcelAddin/PandIDribbon.cs
@@ -11,8 +11,13 @@ namespace PandIDExcelAddin
     {
         private void PandIDribbon_Load(object sender, RibbonUIEventArgs e)
         {
+            //fill the various fields from the SQL database
             fillRibonDropDown(ref dropDownDiagrams, "SELECT name FROM diagrams where useincalcsheet!=0",  "name");
             fillRibonDropDown(ref dropDownProject, "SELECT * FROM projects", "number");
+            RibbonDropDownItem selectedItem = dropDownProject.Items[0];
+            fillLabel(ref labelCustomer, "SELECT customer FROM projects where number=" + selectedItem.Label);
+            fillLabel(ref labelClass, "SELECT classsoc FROM projects where number=" + selectedItem.Label);
+            fillRibonDropDown(ref dropDownFluid, "SELECT DISTINCT type FROM pandid.diagrams ORDER BY type ASC", "type");
         }
 
         private void fillRibonDropDown(ref RibbonDropDown rbControl, string query, string col)
@@ -25,15 +30,81 @@ namespace PandIDExcelAddin
                     using (var reader = command.ExecuteReader())
                     {
                         //Iterate through the rows and add it to the combobox's items
-                        while (reader.Read())
+                        if (reader.HasRows)
                         {
-                            RibbonDropDownItem item = Globals.Factory.GetRibbonFactory().CreateRibbonDropDownItem();
-                            item.Label = reader.GetString(col);
-                            rbControl.Items.Add(item);
+                            while (reader.Read())
+                            {
+                                RibbonDropDownItem item = Globals.Factory.GetRibbonFactory().CreateRibbonDropDownItem();
+                                item.Label = reader.GetString(col);
+                                rbControl.Items.Add(item);
+                            }
                         }
                     }
                 }
             }
+        }
+
+        private void fillLabel(ref RibbonLabel rbLabel, string query)
+        {
+            using (var connection = new MySqlConnection(Properties.Settings.Default.pandidConnectionString))
+            {
+                connection.Open();
+                using (var command = new MySqlCommand(query, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                rbLabel.Label = reader.GetString(0);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void dropDownProject_SelectionChanged(object sender, RibbonControlEventArgs e)
+        {
+            RibbonDropDown rb = (RibbonDropDown)sender;
+            RibbonDropDownItem selectedItem = rb.SelectedItem;
+            fillLabel(ref labelCustomer, "SELECT customer FROM projects where number=" + selectedItem.Label);
+            fillLabel(ref labelClass, "SELECT classsoc FROM projects where number=" + selectedItem.Label);
+        }
+
+        private void dropDownDiagrams_SelectionChanged(object sender, RibbonControlEventArgs e)
+        {
+            RibbonDropDown rb = (RibbonDropDown)sender;
+            string searchstr = rb.SelectedItem.Label;
+            
+            using (var connection = new MySqlConnection(Properties.Settings.Default.pandidConnectionString))
+            {
+                connection.Open();
+                using (var command = new MySqlCommand("SELECT type FROM diagrams where name=\"" + searchstr + "\"", connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                string typestr = reader.GetString(0);
+                                foreach (RibbonDropDownItem li in dropDownFluid.Items)
+                                {
+                                    if (li.Label.CompareTo(typestr) == 0)
+                                    {
+                                        dropDownFluid.SelectedItem = li;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            
+
         }
 
     }
